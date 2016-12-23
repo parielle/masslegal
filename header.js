4e965d930c07441d4da5332a81b10fba32efd026
flowermepink <pariellel@gmail.com>
2016-12-23 14:00:40 -0500

    Create header.js

---
+++ header.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+$("document").ready(function() {
+	"use strict";
+	console.log('here');
+$('#header').html(
+	'<ul><li><a href="home.html">Home</a></li><li><a href="resources.html">Resources</a></li><li>Contact Us</li></ul>');
+});
