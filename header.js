7df3374a481a4d5a27354bf9fe9651a03500752f
flowermepink <pariellel@gmail.com>
2016-12-23 14:02:33 -0500

    Update header.js

--- header.js
+++ header.js
@@ -3,5 +3,5 @@ $("document").ready(function() {
 	"use strict";
 	console.log('here');
 $('#header').html(
-	'<ul><li><a href="home.html">Home</a></li><li><a href="resources.html">Resources</a></li><li>Contact Us</li></ul>');
+	'<ul><li><a href="index.html">Home</a></li><li><a href="resources.html">Resources</a></li><li>Contact Us</li></ul>');
 });
