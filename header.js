63b1c7ac13b20a94a155dc7e4139d99e7ff6e986
flowermepink <pariellel@gmail.com>
2016-12-26 05:59:54 -1000

    Update header.js

--- header.js
+++ header.js
@@ -1,7 +1,6 @@
 // JavaScript Document
 $("document").ready(function() {
 	"use strict";
-	console.log('here');
 $('#header').html(
 	'<ul><li><a href="index.html">Home</a></li><li><a href="resources.html">Resources</a></li><li>Contact Us</li></ul>');
 // &copy;
