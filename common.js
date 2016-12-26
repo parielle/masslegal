6fb4a7e003450d3a05f5f2afa4c3f78c5251524e
flowermepink <pariellel@gmail.com>
2016-12-26 12:47:22 -1000

    Update and rename header.js to common.js

---
+++ common.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+$("document").ready(function() {
+	"use strict";
+$('#header').html(
+	'<ul><li><a href="index.html">Home</a></li><li><a href="resources.html">Resources</a></li><li><a href="contact.html">Contact Us</a></li></ul>');
+// &copy;
+$('#footer').html('Massachusetts Legal ©');
+});
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-89445674-1', 'auto');
+ga('send', 'pageview');
--- header.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-$("document").ready(function() {
-	"use strict";
-$('#header').html(
-	'<ul><li><a href="index.html">Home</a></li><li><a href="resources.html">Resources</a></li><li><a href="contact.html">Contact Us</a></li></ul>');
-// &copy;
-$('#footer').html('Massachusetts Legal ©');
-});
