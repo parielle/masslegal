9f35cbbca48d838bf76d065f361890a63873b27f
flowermepink <pariellel@gmail.com>
2016-12-26 08:54:42 -1000

    Update header.js

--- header.js
+++ header.js
@@ -2,7 +2,7 @@
 $("document").ready(function() {
 	"use strict";
 $('#header').html(
-	'<ul><li><a href="index.html">Home</a></li><li><a href="resources.html">Resources</a></li><li>Contact Us</li></ul>');
+	'<ul><li><a href="index.html">Home</a></li><li><a href="resources.html">Resources</a></li><li><a href="contact.html">Contact Us</a></li></ul>');
 // &copy;
 $('#footer').html('Massachusetts Legal ©');
 });
